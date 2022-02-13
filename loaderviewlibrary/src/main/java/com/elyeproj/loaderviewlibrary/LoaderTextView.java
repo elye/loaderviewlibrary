21f30edddcc5a53682fe0fae021adab13d5f44a7
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2022-02-14 02:03:19 +0530

    let loader views respect 'android:gravity' and 'android:textAlignment'

--- loaderviewlibrary/src/main/java/com/elyeproj/loaderviewlibrary/LoaderTextView.java
+++ loaderviewlibrary/src/main/java/com/elyeproj/loaderviewlibrary/LoaderTextView.java
@@ -16,13 +16,16 @@ package com.elyeproj.loaderviewlibrary;
  * limitations under the License.
  */
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.core.content.ContextCompat;
 
@@ -31,6 +34,7 @@ public class LoaderTextView extends AppCompatTextView implements LoaderView {
     private LoaderController loaderController;
     private int defaultColorResource;
     private int darkerColorResource;
+    private int loaderGravity;
 
     public LoaderTextView(Context context) {
         super(context);
@@ -47,6 +51,7 @@ public class LoaderTextView extends AppCompatTextView implements LoaderView {
         init(attrs);
     }
 
+    @SuppressLint("SwitchIntDef")
     private void init(AttributeSet attrs) {
         loaderController = new LoaderController(this);
         TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.loader_view, 0, 0);
@@ -57,6 +62,23 @@ public class LoaderTextView extends AppCompatTextView implements LoaderView {
         defaultColorResource = typedArray.getColor(R.styleable.loader_view_custom_color, ContextCompat.getColor(getContext(), R.color.default_color));
         darkerColorResource = typedArray.getColor(R.styleable.loader_view_custom_color, ContextCompat.getColor(getContext(), R.color.darker_color));
         typedArray.recycle();
+        loaderGravity = getGravity();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) { // prefer text alignment when available.
+            final int verticalGravity = loaderGravity & Gravity.VERTICAL_GRAVITY_MASK;
+            switch (getTextAlignment()) {
+                case android.view.View.TEXT_ALIGNMENT_CENTER:
+                    loaderGravity = verticalGravity | Gravity.CENTER_HORIZONTAL;
+                    break;
+                case android.view.View.TEXT_ALIGNMENT_TEXT_END:
+                case android.view.View.TEXT_ALIGNMENT_VIEW_END:
+                    loaderGravity = verticalGravity | Gravity.END;
+                    break;
+                case android.view.View.TEXT_ALIGNMENT_TEXT_START:
+                case android.view.View.TEXT_ALIGNMENT_VIEW_START:
+                    loaderGravity = verticalGravity | Gravity.START;
+                    break;
+            }
+        }
     }
 
     @Override
@@ -102,6 +124,11 @@ public class LoaderTextView extends AppCompatTextView implements LoaderView {
     @Override
     public boolean valueSet() {
         return !TextUtils.isEmpty(getText());
+    }
+
+    @Override
+    public int getLoaderGravity() {
+        return loaderGravity;
     }
 
     @Override
